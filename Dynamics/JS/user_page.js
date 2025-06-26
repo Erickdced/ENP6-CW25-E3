4781c758c73d1466b9caeb97a4b63a5cafb7c388
Pedro-MartinezLimon <Pedrolimmon@gmail.com>
2025-06-25 18:32:53 -0600

    Pre pull

--- Dynamics/JS/user_page.js
+++ Dynamics/JS/user_page.js
@@ -1,5 +1,7 @@
 const username = document.getElementById('header_text_user');
-   
+
+const change_username_button = document.getElementById('changeNameBtn'); 
+const change_pfp_button = document.getElementById('changeNameBtn');
 
 document.addEventListener("DOMContentLoaded", ()=>
 {
@@ -8,6 +10,29 @@ document.addEventListener("DOMContentLoaded", ()=>
     else
         username.innerHTML = getUsername();
 
+    /*Modifying username or pfp*/
+    change_username_button.addEventListener("click", ()=>
+    {
+        const change_username_input = document.getElementById('changeNameInput');
+        
+        if (change_username_input.style.display == "block")
+            change_username_input.style.display = "none";
+        else
+            change_username_input.style.display = "block";
 
-    
+        change_username_input.addEventListener("change", ()=>
+        {
+            if(change_username_input.value != '')
+            {
+                setCookie("username", change_username_input.value, 7);
+                username.innerHTML = change_username_input.value;
+            }
+        });
+
+        change_username_input.removeEventListener("change");
+    });
+    change_pfp_button.addEventListener("click", ()=>
+    {
+
+    });
 });
